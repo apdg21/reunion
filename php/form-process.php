9c0c1ca2349d458c9e73c9192b53f330e24ee85f
apdg21 <floda2114@gmail.com>
2024-05-16 23:14:48 +0800

    Update form-process.php

--- php/form-process.php
+++ php/form-process.php
@@ -16,22 +16,6 @@ if (empty($_POST["email"])) {
     $email = $_POST["email"];
 }
 
-// MSG Guest
-if (empty($_POST["guest"])) {
-    $errorMSG .= "Subject is required ";
-} else {
-    $guest = $_POST["guest"];
-}
-
-
-// MSG Event
-if (empty($_POST["event"])) {
-    $errorMSG .= "Subject is required ";
-} else {
-    $event = $_POST["event"];
-}
-
-
 // MESSAGE
 if (empty($_POST["message"])) {
     $errorMSG .= "Message is required ";
@@ -39,8 +23,7 @@ if (empty($_POST["message"])) {
     $message = $_POST["message"];
 }
 
-
-$EmailTo = "armanmia7@gmail.com";
+$EmailTo = "dolfean214@gmail.com";  // Update this to your desired recipient email address
 $Subject = "New Message Received";
 
 // prepare email body text
@@ -51,12 +34,6 @@ $Body .= "\n";
 $Body .= "Email: ";
 $Body .= $email;
 $Body .= "\n";
-$Body .= "guest: ";
-$Body .= $guest;
-$Body .= "\n";
-$Body .= "event: ";
-$Body .= $event;
-$Body .= "\n";
 $Body .= "Message: ";
 $Body .= $message;
 $Body .= "\n";
@@ -66,8 +43,8 @@ $success = mail($EmailTo, $Subject, $Body, "From:".$email);
 
 // redirect to success page
 if ($success && $errorMSG == ""){
-   echo "success";
-}else{
+    echo "success";
+} else {
     if($errorMSG == ""){
         echo "Something went wrong :(";
     } else {
